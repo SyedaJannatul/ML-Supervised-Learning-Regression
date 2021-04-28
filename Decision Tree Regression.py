a0f7f492c1e6f0feefb3acc986e4c3892452c9b3
SYEDA JANNATUL NAIM (MEEM) <79764038+SyedaJannatul@users.noreply.github.com>
2021-04-28 20:53:59 +0600

    regression files

---
+++ Decision Tree Regression.py
@@ -0,0 +1,86 @@
+#Decision Tree Regression
+
+#Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing dataset
+dataset = pd.read_csv("Position_Salaries.csv")
+     #Set matrix of idependent variable
+X=dataset.iloc[:,1:2].values
+    #Set matrix of dependent variable
+Y=dataset.iloc[:,2].values
+
+"""
+#Categorical variable,Dummy variable & One hot encoding
+    #Categorical variable
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,3] = labelencoder_X.fit_transform(X[:,3])
+    #Dummy variable & One hot encoding
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding the Dummy variable Trap
+X = X[:,1:]
+      
+#Spliting the dataset into the training set and test set
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
+"""
+
+"""#Feature Scalimg
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_Y = StandardScaler()
+X = sc_X.fit_transform(X)
+Y= sc_Y.fit_transform(np.reshape(Y,(10,1)))
+"""
+
+"""
+#Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X,Y)
+"""
+
+#Fitting Decision Tree Regression to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X,Y)
+"""
+#Visualizing Linear Regression model
+plt.scatter(X,Y,color = 'red')
+plt.plot(X,lin_reg.predict(X),color = 'blue')
+plt.title("Truth or Bluff(Linear Regression Model)")
+plt.xlabel("Positionlevel")
+plt.ylabel("Salary")
+plt.show()
+"""
+
+#Visualizing Decision Tree Regression model
+plt.scatter(X,Y,color = 'red')
+plt.plot(X,regressor.predict(X),color = 'blue')
+plt.title("Truth or Bluff(Decision Tree Regression Model)")
+plt.xlabel("Positionlevel")
+plt.ylabel("Salary")
+plt.show()
+
+#Visualizing Decision Tree Regression model(for higher resolution and smoother curve)
+X_grid = np.arange(min(X),max(X),.01)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X,Y,color = 'red')
+plt.plot(X_grid,regressor.predict(X_grid),color = 'blue')
+plt.title("Truth or Bluff(Decision Tree Regression Model)")
+plt.xlabel("Positionlevel")
+plt.ylabel("Salary")
+plt.show()
+
+"""
+#Predicting a new result with Linear Regression
+lin_reg.predict([[6.5]])
+"""
+
+#Predicting a new result with Decision Tree Regression
+Y_pred = regressor.predict([[6.5]])
---
+++ Support Vector Regression.py
@@ -0,0 +1,86 @@
+#Polynomial Regression
+
+#Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing dataset
+dataset = pd.read_csv("Position_Salaries.csv")
+     #Set matrix of idependent variable
+X=dataset.iloc[:,1:2].values
+    #Set matrix of dependent variable
+Y=dataset.iloc[:,2].values
+
+"""
+#Categorical variable,Dummy variable & One hot encoding
+    #Categorical variable
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,3] = labelencoder_X.fit_transform(X[:,3])
+    #Dummy variable & One hot encoding
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding the Dummy variable Trap
+X = X[:,1:]
+      
+#Spliting the dataset into the training set and test set
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
+"""
+
+#Feature Scalimg
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_Y = StandardScaler()
+X = sc_X.fit_transform(X)
+Y= sc_Y.fit_transform(np.reshape(Y,(10,1)))
+
+
+"""
+#Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X,Y)
+"""
+
+#Fitting SVR to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X,Y)
+"""
+#Visualizing Linear Regression model
+plt.scatter(X,Y,color = 'red')
+plt.plot(X,lin_reg.predict(X),color = 'blue')
+plt.title("Truth or Bluff(Linear Regression Model)")
+plt.xlabel("Positionlevel")
+plt.ylabel("Salary")
+plt.show()
+"""
+
+"""#Visualizing Polynomial Regression model (for higher resolution and smoother curve)
+X_grid = np.arange(min(X),max(X),.1)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X,Y,color = 'red')
+plt.plot(X_grid,regressor.predict(sc_X.fit_transform(X_grid)),color = 'blue')
+plt.title("Truth or Bluff(SVR Model)")
+plt.xlabel("Positionlevel")
+plt.ylabel("Salary")
+plt.show()"""
+
+#Visualizing SVR model
+plt.scatter(X,Y,color = 'red')
+plt.plot(X,regressor.predict(X),color = 'blue')
+plt.title("Truth or Bluff(SVR Model)")
+plt.xlabel("Positionlevel")
+plt.ylabel("Salary")
+plt.show()
+
+"""
+#Predicting a new result with Linear Regression
+lin_reg.predict([[6.5]])
+"""
+
+#Predicting a new result with SVR
+Y_pred = sc_Y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
